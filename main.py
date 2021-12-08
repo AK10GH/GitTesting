9a59bb28860645623931627cef969ea6b4e72cae
Anonymous007 <95375499+AK10GH@users.noreply.github.com>
2021-12-08 22:04:16 +0200

    Update main.py
    This function deducts two numbers from each other, changed "add" to "deduct" and + to -

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# function that adds two numbers together
-def add(number1,number2):
-  return int(number1) + int(number2)
+# function that deducts two numbers from each other
+def deduct(number1,number2):
+  return int(number1) - int(number2)
 number1 = "300"
 number2 = "200"
-print(add(number1,number2))
+print(deduct(number1,number2))
